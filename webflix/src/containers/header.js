9c8d5adacb69b08cd2fe7ede2bd271c83a97d334
Grant Belcher <gtbelch@gmail.com>
2022-05-09 18:21:42 -0700

    adds logo svg to header

--- webflix/src/containers/header.js
+++ webflix/src/containers/header.js
@@ -1,11 +1,12 @@
 import { Header } from "../components";
+import logo from "../logo.svg";
 
 export function HeaderContainer() {
   console.log(Header.Frame, "look here");
   return (
     <Header>
       <Header.Frame>
-        <Header.Logo to="/" />
+        <Header.Logo to="/" alt="Webflix" src={logo} />
         <Header.ButtonLink to="/">Test</Header.ButtonLink>
       </Header.Frame>
     </Header>
