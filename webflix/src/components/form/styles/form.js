29a13746aaa59b7d0c8e83bf3b154fd7998f1f44
Grant Belcher <gtbelch@gmail.com>
2022-05-10 21:05:29 -0700

    implements basic form state for sign in page

--- webflix/src/components/form/styles/form.js
+++ webflix/src/components/form/styles/form.js
@@ -2,8 +2,10 @@ import styled from "styled-components/macro";
 import { Link as ReachRouterLink } from "react-router-dom";
 
 export const Container = styled.div``;
-export const Base = styled.div``;
+export const Base = styled.form``;
 export const Error = styled.div``;
+export const Title = styled.h1``;
+
 export const Text = styled.p``;
 export const TextSmall = styled.p``;
 
--- webflix/src/pages/signin.js
+++ webflix/src/pages/signin.js
@@ -5,9 +5,47 @@ import { HeaderContainer } from "../containers/header";
 import { Form } from "../components";
 
 export default function SignIn() {
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  // check form input elements are valid
+  const isInvalid = password === "" || emailAddress === "";
+  // email and password
+  const handleSignin = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <>
-      <HeaderContainer></HeaderContainer>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+          <Form.Base onSubmit={handleSignin} method="POST">
+            <Form.Input
+              placeholder="email"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              type="password"
+              placeholder="password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit
+              disabled={isInvalid}
+              type="submit"
+              onClick={() => console.log(emailAddress, password)}
+            >
+              Submit
+            </Form.Submit>
+          </Form.Base>
+          <Form.Text>form text</Form.Text>
+          <Form.TextSmall>form text small</Form.TextSmall>
+        </Form>
+      </HeaderContainer>
       <FooterContainer />
     </>
   );
