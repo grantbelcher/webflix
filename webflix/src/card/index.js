5c2eb6d8384d145e1423c40ff364a5e842f0e8ff
Grant Belcher <gtbelch@gmail.com>
2022-05-14 08:45:40 -0700

    outlines Card component

---
+++ webflix/src/card/index.js
@@ -0,0 +1,73 @@
+import React, { useState, useContext, createContext } from "react";
+
+import {
+  Container,
+  Group,
+  Title,
+  Subtitle,
+  Text,
+  Feature,
+  FeatureTitle,
+  FeatureText,
+  FeatureClose,
+  Maturity,
+  Content,
+  Meta,
+  Entities,
+  Item,
+  Image,
+} from "./styles/card";
+
+export const FeatureContext = createContext();
+
+export default function Card({ children, ...restProps }) {
+  const [showFeature, setShowFeature] = useState(false);
+  const [itemFeature, setItemFeature] = useState({});
+
+  return (
+    <FeatureContext.Provider
+      value={{ showFeature, setShowFeature, itemFeature, setItemFeature }}
+    >
+      <Container {...restProps}>{children}</Container>
+    </FeatureContext.Provider>
+  );
+}
+// holds all cards
+Card.Group = function CardGroup({ children, ...restProps }) {
+  return <Group {...restProps}>{children}</Group>;
+};
+
+Card.Entities = function CardEntities({ children, ...restProps }) {
+  return <Entities {...restProps}>{children}</Entities>;
+};
+
+// titles will display genres
+Card.Title = function CardTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+};
+Card.Subtitle = function CardSubtitle({ children, ...restProps }) {
+  return <Subtitle {...restProps}>{children}</Subtitle>;
+};
+
+Card.Meta = function CardMeta({ children, ...restProps }) {
+  return <Meta {...restProps}>{children}</Meta>;
+};
+
+Card.Item = function CardItem({ item, children, ...restProps }) {
+  const { setShowFeature, setItemFeature } = useContext(FeatureContext);
+  return (
+    <Item
+      onClick={() => {
+        setItemFeature(item);
+        setShowFeature(true);
+      }}
+      {...restProps}
+    >
+      {children}
+    </Item>
+  );
+};
+
+Card.Image = function CardImage({ ...restProps }) {
+  return <Image {...restProps} />;
+};
