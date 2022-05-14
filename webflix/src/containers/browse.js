3324417fef4484b281d7cb366a58457b061b1907
Grant Belcher <gtbelch@gmail.com>
2022-05-14 13:19:04 -0700

    creates Player component and adds to Feature on browse page

--- webflix/src/containers/browse.js
+++ webflix/src/containers/browse.js
@@ -2,7 +2,9 @@ import React, { useState, useContext, useEffect } from "react";
 import { SelectProfileContainer } from "./profile";
 import { FooterContainer } from "./footer";
 import { FirebaseContext } from "../context/firebase";
+// move these next twoto index.js
 import Loading from "../components/loading";
+import Player from "../components/player";
 import { Card, Header } from "../components";
 import * as ROUTES from "../constants/routes";
 import logo from "../logo.svg";
@@ -101,11 +103,10 @@ export function BrowseContainer({ slides }) {
                 })}
               </Card.Entities>
               <Card.Feature category={category}>
-                <p>hello from card feature</p>
-                {/* <Player>
+                <Player>
                   <Player.Button />
                   <Player.Video src="/videos/bunny.mp4" />
-                </Player> */}
+                </Player>
               </Card.Feature>
             </Card>
           );
